58d63525004ddf4ef1d04351ade6da73b6426c89
Kiefer <mail@mail.com>
2024-02-01 09:01:27 -0800

    HTML:
    - Added an Index.html
    
    JS:
    
    - Modified current Func() for Index.html

---
+++ static/js/scriptTemplate.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Change Avatar on mouseover
+    let svgAvatar = document.getElementById('svgAvatar');
+    svgAvatar.addEventListener('mouseover', function () {
+        svgAvatar.src = 'static/img/ironman-av.svg';
+    });
+    svgAvatar.addEventListener('mouseout', function () {
+        svgAvatar.src = 'static/img/batman-av.svg';
+    });
+});
