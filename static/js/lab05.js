3bb13896a58b6ecb1f728967a390c6bc509d12b0
Kiefer <mail@mail.com>
2024-02-23 18:25:10 -0800

    Remove hardcoded default values from form fields
    Removed preset values from form fields to make the fields appear empty by default, enhancing user interaction. Also, adjusted age validation checks to include non-numeric entries, ensuring that the age field only accepts valid numeric inputs.

--- static/js/lab05.js
+++ static/js/lab05.js
@@ -49,7 +49,7 @@ const initializeLogin = () => {
                             </svg>
                         </span>
                         <input type="text" class="form-control" id="first-name" placeholder="Enter your first name..."
-                               required value="Kiefer" pattern="^[A-Za-z]+$">
+                               required pattern="^[A-Za-z]+$">
                     </div>
                     <div class="mb-3 input-group">
                         <label for="last-name" class="form-label" hidden>Last Name</label>
@@ -61,7 +61,7 @@ const initializeLogin = () => {
                             </svg>
                         </span>
                         <input type="text" class="form-control" id="last-name" placeholder="Enter your last name..."
-                               required value="Kiefer" pattern="^[A-Za-z]+$">
+                               required pattern="^[A-Za-z]+$">
                     </div>
                     <div class="mb-3 input-group">
                         <label for="age" class="form-label" hidden>Age</label>
@@ -71,7 +71,7 @@ const initializeLogin = () => {
                               <path d="M8 1a.5.5 0 0 1 .5.5v5.243L9 7.1V4.72C9 3.77 9.77 3 10.72 3c.524 0 1.023.27 1.443.592.431.332.847.773 1.216 1.229.736.908 1.347 1.946 1.58 2.48.176.405.393 1.16.556 2.011.165.857.283 1.857.24 2.759-.04.867-.232 1.79-.837 2.33-.67.6-1.622.556-2.741-.004l-1.795-.897A2.5 2.5 0 0 1 9 11.264V8.329l-1-.715-1 .715V7.214c-.1 0-.202.03-.29.093l-2.5 1.786a.5.5 0 1 0 .58.814L7 8.329v2.935A2.5 2.5 0 0 1 5.618 13.5l-1.795.897c-1.12.56-2.07.603-2.741.004-.605-.54-.798-1.463-.838-2.33-.042-.902.076-1.902.24-2.759.164-.852.38-1.606.558-2.012.232-.533.843-1.571 1.579-2.479.37-.456.785-.897 1.216-1.229C4.257 3.27 4.756 3 5.28 3 6.23 3 7 3.77 7 4.72V7.1l.5-.357V1.5A.5.5 0 0 1 8 1m3.21 8.907a.5.5 0 1 0 .58-.814l-2.5-1.786A.5.5 0 0 0 9 7.214V8.33z"/>
                             </svg>
                         </span>
-                        <input type="number" class="form-control" id="age" placeholder="Enter your age..." required value="25" min="0" max="120">
+                        <input type="number" class="form-control" id="age" placeholder="Enter your age..." required min="0" max="120">
                     </div>
                     <div class="mb-3 input-group">
                         <label for="email" class="form-label" hidden>Email address</label>
@@ -82,7 +82,7 @@ const initializeLogin = () => {
                               <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5.5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0zM2 5.5a.5.5 0 0 0 .5.5H6a.5.5 0 0 0 0-1H2.5a.5.5 0 0 0-.5.5M2.5 7a.5.5 0 0 0 0 1H6a.5.5 0 0 0 0-1zM2 9.5a.5.5 0 0 0 .5.5H6a.5.5 0 0 0 0-1H2.5a.5.5 0 0 0-.5.5m8-4v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5"/>
                             </svg>
                         </span>
-                        <input type="email" class="form-control" id="email" placeholder="Enter your email..." required value="k@k.com" pattern="[^@\\s]+@[^@\\s]+\\.[^@\\s]+">
+                        <input type="email" class="form-control" id="email" placeholder="Enter your email..." required pattern="[^@\\s]+@[^@\\s]+\\.[^@\\s]+">
                     </div>
                     <div class="mb-3 input-group">
                         <label for="postal-code" class="form-label" hidden>Postal Code</label>
@@ -93,7 +93,7 @@ const initializeLogin = () => {
                             </svg>
                         </span>
                         <input type="text" class="form-control" id="postal-code" placeholder="Enter your postal..."
-                               required value="v9z0z1" pattern="^[A-Za-z]\\d[A-Za-z] ?\\d[A-Za-z]\\d$">
+                               required pattern="^[A-Za-z]\\d[A-Za-z] ?\\d[A-Za-z]\\d$">
                     </div>
                     <div class="mb-3 input-group">
                         <label for="phone-number" class="form-label" hidden>Phone Number</label>
@@ -104,7 +104,7 @@ const initializeLogin = () => {
                             </svg>
                         </span>
                         <input type="tel" class="form-control" id="phone-number"
-                               placeholder="Enter your phone number..." required value="250 880 1125" pattern="^(?:\\d{3}-\\d{3}-\\d{4}|\\d{10}|\\d{3} \\d{3} \\d{4})$">
+                               placeholder="Enter your phone number..." required pattern="^(?:\\d{3}-\\d{3}-\\d{4}|\\d{10}|\\d{3} \\d{3} \\d{4})$">
                     </div>
                 </form>
                 <div class="d-flex flex-column justify-content-center mb-3" id="register-response">
@@ -317,7 +317,7 @@ const validateForm = (firstName, lastName, phoneNumber, email, age, postalCode) 
         validationMessages.push('Invalid email format. Use email@email.com');
     }
 
-    if (age < 0 || age > 120) {
+    if (age < 0 || age > 120 || !/^\d+$/.test(age)) {
         validationMessages.push('Invalid age. Must be between 0 and 120.');
     }
 
